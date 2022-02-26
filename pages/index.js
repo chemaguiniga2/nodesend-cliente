3c981b234aa7da578fd8ec94e4efcfe857452be9
chemaguiniga2 <A01376669@itesm.mx>
2022-02-26 12:04:01 -0600

    creating dropzone component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
+import Link from 'next/link';
 import React, { useContext, useEffect } from 'react';
 import Layout from '../components/Layout';
 import authContext from '../context/auth/authContext';
+import Dropzone from '../components/Dropzone';
 
 
 const Index = () => {
@@ -15,7 +17,22 @@ const Index = () => {
 
   return (
     <Layout>
-      <h1>Index</h1>
+      <div className='md:w-4/5 xl:w-3/5 mx-auto mb-32'>
+        <div className='lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10'>
+          <Dropzone />
+          <div className='md:flex-1 mb-3 mx-2 mt-16 lg:mt-0'>
+            <h2 className='text-4xl font-sans font-bold text-gray-800 my-4'>
+              Compartir archivos de forma sencilla y privada
+            </h2>
+            <p className='text-lg leading-loose'>
+              <span className='text-red-500 font-bold'>ReactNodeSend</span> te permite compartir archivos con cifrado extremo a extremo y un archivo que es eliminado despuès de ser descargado. Asì que puedes mantener lo que compartes en privado y asegurarte de que tus cosas no permanezcan en línea para siempre.
+            </p>
+            <Link href="/crearcuenta">
+              <a className='text-red-500 font-bold text-lg hover:text-red-700'>Crea una cuenta para mayores beneficios</a>
+            </Link>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
